1bfb221face86592caca12baab34271ef4fca8db
ch2isk4kos <ch2isk4kos@gmail.com>
2023-12-20 11:59:54 -0500

    add structural styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,35 +4,32 @@ const Navbar = () => {
   return (
     <nav className="Navbar">
       <h1 className="logo">LOGO</h1>
-      <ul>
-        <li className="link">
-          <a
-            href="https://chriskakos.medium.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            MD
-          </a>
-        </li>
-        <li className="link">
-          <a
-            href="https://linkedin.com/in/chris-kakos"
-            target="_blank"
-            rel="noreferrer"
-          >
-            LI
-          </a>
-        </li>
-        <li className="link">
-          <a
-            href="https://www.github.com/ch2isk4kos"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GH
-          </a>
-        </li>
-      </ul>
+      <div className="links">
+        <a
+          className="link"
+          href="https://chriskakos.medium.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          MD
+        </a>
+        <a
+          className="link"
+          href="https://linkedin.com/in/chris-kakos"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LI
+        </a>
+        <a
+          className="link"
+          href="https://www.github.com/ch2isk4kos"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GH
+        </a>
+      </div>
     </nav>
   );
 };
