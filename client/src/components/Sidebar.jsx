9f4bc6c7010e2e6791efdbc1736fb88477aec961
ch2isk4kos <ch2isk4kos@gmail.com>
2023-12-20 10:14:27 -0500

    add sidebar sections

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,9 +3,17 @@ import "../styles/sidebar.css";
 const Sidebar = () => {
   return (
     <div className="Sidebar">
-      <p>Link</p>
-      <p>Link</p>
-      <p>Link</p>
+      <section className="open-chats"></section>
+      <section className="settings">
+        <div className="bottom-left">
+          <p>Link</p>
+          <p>Link</p>
+          <p>Link</p>
+        </div>
+        <div className="bottom-right">
+          <p>Link</p>
+        </div>
+      </section>
     </div>
   );
 };
