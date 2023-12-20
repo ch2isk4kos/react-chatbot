23c58b7698674534f5e94ba7511459ea28f9caf0
ch2isk4kos <ch2isk4kos@gmail.com>
2023-12-20 10:44:01 -0500

    create navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import "../styles/navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="Navbar">
+      <ul>
+        <li className="link">
+          <a
+            href="https://chriskakos.medium.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            MD
+          </a>
+        </li>
+        <li className="link">
+          <a
+            href="https://linkedin.com/in/chris-kakos"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LI
+          </a>
+        </li>
+        <li className="link">
+          <a
+            href="https://www.github.com/ch2isk4kos"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GH
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
