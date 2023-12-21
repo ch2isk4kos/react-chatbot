92ff878db76c032fd957a641209052d6bf9374d5
ch2isk4kos <ch2isk4kos@gmail.com>
2023-12-21 11:49:55 -0500

    import/config fontawesome icons to render globally

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
+import "./App.css";
 import Chatbox from "./components/Chatbox";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
-library.add(faHouse);
-import "./App.css";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faLinkedinIn,
+  faMedium,
+} from "@fortawesome/free-brands-svg-icons";
+library.add(fas, faGithub, faLinkedinIn, faMedium);
 
 function App() {
   return (
