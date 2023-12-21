6e936be4afe3e9b18a99f31eb7cda2530304d0de
ch2isk4kos <ch2isk4kos@gmail.com>
2023-12-21 11:23:57 -0500

    install fontawesome dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import Chatbox from "./components/Chatbox";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+library.add(faHouse);
 import "./App.css";
 
 function App() {
