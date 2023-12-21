2d96297ffe5cee4bf56cc32927d295d85fe46fd7
ch2isk4kos <ch2isk4kos@gmail.com>
2023-12-21 10:29:15 -0500

    add structural styling

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,15 @@
 import "../styles/sidebar.css";
 
 const Sidebar = () => {
+  const handleOnNewChat = (e) => {
+    e.preventDefault();
+    console.log("we have a click!");
+  };
   return (
     <div className="Sidebar">
-      <div className="open-chats"></div>
+      <div className="open-chats">
+        <div className="chat" onClick={(e) => handleOnNewChat(e)}></div>
+      </div>
       <div className="settings">
         <div className="bottom-left">
           <p className="option">Link</p>
