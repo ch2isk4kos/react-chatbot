4fc16c7e4510e28d004799d811c6a9c4e722950e
ch2isk4kos <ch2isk4kos@gmail.com>
2023-12-21 16:51:29 -0500

    add tertiary operator to render either sidebar or hamburger menu depending on value of isActive

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,27 +1,63 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../styles/sidebar.css";
 
 const Sidebar = () => {
+  const [isActive, setIsActive] = useState(true);
+
   const handleOnNewChat = (e) => {
     e.preventDefault();
     console.log("we have a click!");
   };
+
+  const handleOnMenu = () => {
+    setIsActive(!isActive);
+  };
+
+  const handleOnHideMenu = () => {
+    setIsActive(true);
+  };
+
   return (
-    <div className="Sidebar">
-      <div className="open-chats">
-        <div className="chat" onClick={(e) => handleOnNewChat(e)}></div>
-      </div>
-      <div className="settings">
-        <div className="bottom-left">
-          <p className="option">Link</p>
-          <p className="option">Link</p>
-          <p className="option">Link</p>
+    <>
+      {isActive ? (
+        <div className="Sidebar">
+          <div className="open-chats">
+            <div className="chat" onClick={(e) => handleOnNewChat(e)}></div>
+          </div>
+          <div className="settings">
+            <div className="bottom-left">
+              <a className="option">
+                <FontAwesomeIcon icon="fa-solid fa-plus" />
+              </a>
+              <a className="option">
+                <FontAwesomeIcon icon="fa-solid fa-minus" />
+              </a>
+              <a className="option">
+                <FontAwesomeIcon icon="fa-solid fa-gear" />
+              </a>
+            </div>
+            <div className="bottom-right">
+              <a className="hamburger" onClick={handleOnMenu}>
+                <FontAwesomeIcon icon="fa-solid fa-bars" />
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="bottom-right">
-          <p className="hamburger">Link</p>
+      ) : (
+        <div className="hamburger-menu">
+          <div className="top">
+            <a className="close-btn" onClick={handleOnHideMenu}>
+              <FontAwesomeIcon icon="fa-solid fa-x" />
+            </a>
+            <h1>Menu</h1>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
-
+{
+  /* <div className={`hamburger-menu ${isActive ? "hide-hamburger-menu" : ""}`}></div> */
+}
 export default Sidebar;
