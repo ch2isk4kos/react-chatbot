59494d171942c6781f311e39acf4f1b8112a7191
ch2isk4kos <ch2isk4kos@gmail.com>
2023-12-22 20:13:32 -0500

    import/config fontawesome icon(s)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,41 @@ import Chatbox from "./components/Chatbox";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import {
+  fas,
+  faBars,
+  faCircleInfo,
+  faDownload,
+  faFileExport,
+  faGears,
+  faMinus,
+  faPlus,
+  faTerminal,
+  faX,
+} from "@fortawesome/free-solid-svg-icons";
 import {
   faGithub,
   faLinkedinIn,
   faMedium,
+  faReact,
 } from "@fortawesome/free-brands-svg-icons";
-library.add(fas, faGithub, faLinkedinIn, faMedium);
+
+library.add(
+  fas,
+  faBars,
+  faCircleInfo,
+  faDownload,
+  faFileExport,
+  faGears,
+  faGithub,
+  faLinkedinIn,
+  faMedium,
+  faMinus,
+  faPlus,
+  faReact,
+  faTerminal,
+  faX
+);
 
 function App() {
   return (
